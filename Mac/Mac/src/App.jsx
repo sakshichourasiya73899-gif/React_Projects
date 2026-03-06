c8e440abbad4dcb281755d8930d64e49ed8430f5
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-03-07 02:59:20 +0530

    Mac Animation integerations

---
+++ Mac/Mac/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.scss"
+import Doc from "./Components/Doc.jsx"
+
+const App = () => {
+  return (
+   <main>
+    <Doc/>
+   </main>
+  )
+}
+
+export default App
---
+++ Mac/Mac/src/Components/Doc.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import "./Doc.scss"
+
+const Doc = () => {
+  return (
+    
+  <footer className='doc'>
+    
+    <div className="icon calender"> <img src="/doc-icon/calender.svg" alt=""/></div>
+    <div className="icon note"> <img src="/doc-icon/note.svg" alt="" /></div>
+    <div className="icon pdf"> <img src="/doc-icon/pdf.svg" alt="" /></div>
+    <div className="icon  github"> <img src="/doc-icon/github.svg" alt="" /></div>
+    <div className="icon spotify"> <img src="/doc-icon/spotify.svg" alt="" /></div>
+    <div className="icon cli"> <img src="/doc-icon/cli.svg" alt="" /></div>
+    <div className="icon mail"> <img src="/doc-icon/mail.svg" alt="" /></div>
+    <div className="icon link"> <img src="/doc-icon/link.svg" alt="" /></div>
+
+  </footer>
+  )
+}
+
+export default Doc
